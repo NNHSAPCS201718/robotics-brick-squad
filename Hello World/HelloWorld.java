bc891ce80482743d9f5c014347babcd5c9c2b173
nbbonney <31215755+nbbonney@users.noreply.github.com>
2018-05-17 09:33:17 -0500

    day 1

--- Hello World/HelloWorld.java
+++ Hello World/HelloWorld.java
@@ -4,13 +4,13 @@
  * @author gcschmit
  * @version 15 May 2018
  */
-import lejos.nxt.Button;
-
-public class Hello
+import lejos.nxt.*;
+public class HelloWorld
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World" );
         Button.waitForAnyPress();
+        Motor.A.forward();
     }
 }
